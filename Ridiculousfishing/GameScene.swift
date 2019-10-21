55c55463fa12b1f774f3efc2c1e4651ef28b1472
prudhvi <prudhvi.satram1995@gmail.com>
2019-10-21 02:48:45 -0400

    created a spawnfish array

--- Ridiculousfishing/GameScene.swift
+++ Ridiculousfishing/GameScene.swift
@@ -13,33 +13,46 @@ class GameScene: SKScene {
     
     
     
-    var boatnode: SKSpriteNode!
-    var hook: SKSpriteNode!
-  
+    var play: SKSpriteNode!
+    var hanger: SKSpriteNode!
+    
+    
     
 
     
     override func didMove(to view: SKView) {
         
-        self.hook = self.childNode(withName: "hook") as! SKSpriteNode
-               self.boatnode = self.childNode(withName: "boat") as! SKSpriteNode
+          self.hook = self.childNode(withName: "hook") as! SKSpriteNode
+                    self.boatnode = self.childNode(withName: "boatnode") as! SKSpriteNode
         
         
     }
-
-
+     var fish:[SKSpriteNode] = []
+            func spawnfish() {
+            let cat:SKSpriteNode = SKSpriteNode(imageNamed: "img")
+               
+               // generate a random x position and y pos
+               let randomXPos = CGFloat.random(in: -700 ... 0)
+               let randomYPos = CGFloat.random(in: 500 ... size.height/2-50)
+               cat.position = CGPoint(x:randomXPos, y:randomYPos)
+               
+               // add the cat to the screen
+               addChild(cat)
+               
+               // add the cat to the array
+               self.fish.append(cat)
+               
            }
-   
     
     override func update(_ currentTime: TimeInterval) {
-  
-        
-        }
+    }
     
  
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-     
+    
+               
+           }
         
         
         
